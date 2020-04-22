2fbb5fde1926807013e9edc5b3d9911517aa9f75
Chen Lin <CLin4@slb.com>
2020-04-21 23:11:39 -0700

    add the first my module with test

--- my_hello/go.mod
+++ my_hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/csharpknife2002/golang/my_hello
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.0
--- my_hello/my_hello.go
+++ my_hello/my_hello.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/csharpknife2002/golang/my_hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Printf("hello, world from me\n")
+	fmt.Printf(morestrings.ReverseString("I am Chen LIN"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
-
